ebcd2bb999272ea2bb3f318902138c79c556ea60
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-02-25 17:44:20 -0600

    main styling done.

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 const startBtn = $('#timerStart');
 const choiceBtn = $('#guessBtn');
 const timerEl = $('#countdown'); 
-const otText = $('timer-text')
+const otText = $('#timer-text')
+
+//start blink
+
+function blink_text() {
+    otText.fadeOut(500);
+    otText.fadeIn(500);
+}
+setInterval(blink_text, 1000);
 
 //start timer
 startBtn.click( function() {
+    
+    $('.timer-element').text('time left: ').append(timerEl);
     let counter = 60;
     setInterval(function() {
         counter--;
         if(counter >= 0) {
         timerEl.text(counter)
         }
-        if(counter === 0) {
-        otText.text('game over');
-        clearInterval(counter);
+
+        if(counter <= 10) {
+            $('.intro').text('hurry up')
         }
-    }, 10);
+
+        if(counter <= 0) {
+            $('.intro').text('game over')
+
+            clearInterval(counter);
+        }
+    }, 1000);
 })
+
