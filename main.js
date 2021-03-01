7218f12f6668a1de04e9598690a810d6c63e8bad
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-02-28 18:49:52 -0600

    rebuild start game function

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 //game variables
-const startBtn = $('#timerStart');
-const nextBtn = $('#nextQuestion');
-const questionEl = $('.question-container');
-const questionText = $('.question')
-const choiceBtn = $('.choice-btn');
+const startBtn = $('#start-btn');
+const nextBtn = $('#next-btn');
+const questionContainer = $('#question-container');
+const questionText = $('#question')
+const choiceBtn = $('#answer-buttons');
 let shuffQuestions, currentQuestionIndex
 
 //timer variables
@@ -56,19 +56,18 @@ startBtn.click( function() {
 
 //this button will move us to the next question after we make a slection
 nextBtn.click( () => {
-    console.log("next question check")
     currentQuestionIndex++
     nextQuestion()
 })
 
 //begin actual game
 function startQuiz() {
+    startBtn.addClass('hide')
+    questionContainer.removeClass('hide')
     shuffQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    startBtn.addClass('hide')
-    nextBtn.removeClass('hide')
-    questionEl.removeClass('hide')
     nextQuestion()
+
 
 }
 
@@ -77,58 +76,10 @@ function nextQuestion() {
     showQuestion(shuffQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if(answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        choiceBtn.append(button)
-    })
-}
 
-function selectAnswer(e) {
-    const selectedButon = e.target
-    const correct = selectedButon.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(choiceBtn.children()).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if(shuffQuestions.length > currentQuestionIndex + 1) {
-        nextBtn.removeClass('hide')
-    } else {
-        startBtn.innerText = 'Restart'
-        startBtn.removeClass('hide')
-    }
-    nextBtn.removeClass('hide')
-}
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if(correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
 
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
 
-function resetState() {
-    clearStatusClass(document.body)
-    nextBtn.addClass('hide')
-    while (choiceBtn.firstChild) {
-        choiceBtn.removeChild
-        (choiceBtn.firstChild)
-    }
-}
 
 
 
@@ -136,31 +87,30 @@ const questions = [
     {
         question: 'what is 2 + 2??',
         answers: [
-            {text: 'i have no idea', correct: true},
-            {text: 'it could be anything', correct: false},
-            {text: 'ummmmmmmmmm, what?', correct: false},
-            {text: 'yoyoyoyoyoyo', correct: false}
+            {text: '4', correct: true},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
         ]
     },
 
     {
         question: 'what is 4 + 4??',
         answers: [
-            {text: 'yeah once again, no idea', correct: true},
-            {text: 'egg time', correct: false},
-            {text: 'egg time', correct: false},
-            {text: 'egg time', correct: false},
+            {text: '8', correct: true},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
         ]
     },
 
     {
-        question: 'what is up my dude',
+        question: 'what is 6 + 6??',
         answers: [
-            {text: 'lol', correct: true},
-            {text: 'omg', correct: false},
-            {text: 'jaja', correct: false},
-            {text: 'omfgggggg', correct: false},
+            {text: '12', correct: true},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
+            {text: '22', correct: false},
         ]
     },
 ]
-
